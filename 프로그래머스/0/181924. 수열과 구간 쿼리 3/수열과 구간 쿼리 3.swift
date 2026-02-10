71cb1e42dcf0a52bcfb7df32b29915a7afd899a3
andev <123827577+andevv@users.noreply.github.com>
2026-02-10 17:56:43 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.34 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+    
+    var answer = arr
+    
+    for i in queries {
+        answer.swapAt(i[0], i[1])
+    }
+    
+    return answer
+}
