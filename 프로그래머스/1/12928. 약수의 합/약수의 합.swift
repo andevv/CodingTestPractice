f2d4e5f0029bb3eab6806f9bd5455563b793aebc
andev <123827577+andevv@users.noreply.github.com>
2026-04-02 20:09:55 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -0,0 +1,13 @@
+func solution(_ n:Int) -> Int {
+    
+    var answer = 0
+    
+    if n == 0 { return 0 }
+    
+    for i in 1...n {
+        if n % i == 0 {
+            answer += i
+        }
+    }
+    return answer
+}
