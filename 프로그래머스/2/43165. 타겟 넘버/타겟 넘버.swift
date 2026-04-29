f2b2f9696e010289cb0d12bd3977e6c14a791153
andev <123827577+andevv@users.noreply.github.com>
2026-04-29 19:04:21 +0900

    [level 2] Title: 타겟 넘버, Time: 39.99 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ numbers: [Int], _ target: Int) -> Int {
+    var count = 0
+    
+    func dfs(_ index: Int, _ sum: Int) {
+        if index == numbers.count {
+            if sum == target {
+                count += 1
+            }
+            return
+        }
+        
+        dfs(index + 1, sum + numbers[index])
+        dfs(index + 1, sum - numbers[index])
+    }
+    
+    dfs(0, 0)
+    
+    return count
+}
