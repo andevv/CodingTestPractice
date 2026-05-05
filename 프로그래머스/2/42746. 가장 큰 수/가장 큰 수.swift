0ebac81018ac77004e2f6f459ae58b1e7dc98ff1
andev <123827577+andevv@users.noreply.github.com>
2026-05-05 22:49:01 +0900

    [level 2] Title: 가장 큰 수, Time: 350.85 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,12 +1,9 @@
 import Foundation
 
-func solution(_ numbers:[Int]) -> String {
-    
-    let strNumbers = numbers.map { String($0) }
-    
-    let sortedNumbers = strNumbers.sorted { $0 + $1 > $1 + $0 }
-    
-    let answer = sortedNumbers.joined()
+func solution(_ numbers: [Int]) -> String {
+    let str = numbers.map { String($0) }
+    let sorted = str.sorted { ($0 + $1) > ($1 + $0) }
+    let answer = sorted.joined()
     
     return answer.first == "0" ? "0" : answer
 }
