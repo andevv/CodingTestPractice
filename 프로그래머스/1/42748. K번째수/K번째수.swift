9b3955fd94e7a93ea6074d4c8679ffbd2e3b2f01
andev <123827577+andevv@users.noreply.github.com>
2026-05-05 20:20:24 +0900

    [level 1] Title: K번째수, Time: 0.15 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,17 +1,16 @@
 import Foundation
 
-func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    
+func solution(_ array: [Int], _ commands: [[Int]]) -> [Int] {
     var answer: [Int] = []
     
-    for i in commands {
-        let start = i[0]
-        let end = i[1]
-        let k = i[2]
-        var slice = array[start-1...end-1]
-        let sort = slice.sorted()
+    for i in 0..<commands.count {
+        let start = commands[i][0]
+        let end = commands[i][1]
+        let k = commands[i][2]
         
-        answer.append(sort[k-1])
+        let slice = array[start-1...end-1]
+        let sorted = slice.sorted()
+        answer.append(sorted[k-1])
     }
     
     return answer
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 0.21 ms
+메모리: 16.8 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 15일 19:32:17
+2026년 05월 05일 20:20:20
 
 ### 문제 설명
 
